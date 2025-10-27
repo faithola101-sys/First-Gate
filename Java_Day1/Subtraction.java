570293a92dc02d30e65a5f8578001442b6e51f01
faithola101-sys <faithola101@gmail.com>
2025-10-27 14:25:01 +0100

    i created a new folder phase gate

---
+++ Java_Day1/Subtraction.java
@@ -0,0 +1,14 @@
+public class Subtraction {
+     public static void main(String[] args) {
+     
+     int sub = 0;
+     
+     for (int i = 0; i < 5; i++) {
+
+     sub = sub - i;
+     
+     System.out.println(Math.abs(sub));
+     
+    }
+  }
+}
